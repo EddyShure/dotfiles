c95e0d02ed7d0f16a9d804454f67fe5c27af9410
Eddy Shure <github@monoton.space>
2019-04-09 09:32:34 +0200

    Make font size in terminal smaller

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono for Powerline:style=regular:pixelsize=17:antialias=true:autohint=false";
+static char *font = "Fira Mono for Powerline:style=regular:pixelsize=16:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
