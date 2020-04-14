c0d8757d27f3e15b368c59e6e25a0fedb976c85b
Niklas Kunz <git@monoton.space>
2020-04-14 15:23:58 +0200

    Add alacritty configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,41 +19,34 @@
   (package-install 'use-package)
   (package-install 'diminish))
 
-;; 
-(eval-when-compile
-  (require 'use-package))
-; (require 'diminish)
+;; Requires use-package
+(eval-when-compile (require 'use-package))
+(require 'diminish)
 (require 'bind-key)
 
-;; Loads conifguration .org file
+;; Loads configuration .org file
 (require 'org)
-(org-babel-load-file (expand-file-name (concat user-emacs-directory "configuration.org")))
-(org-babel-load-file (expand-file-name "~/dotfiles/private/emacs/mu4e-contexts.org"))
-(org-babel-load-file (expand-file-name "~/dotfiles/private/emacs/rss-feeds.org"))
+(org-babel-load-file 
+  (expand-file-name (concat user-emacs-directory "configuration.org")))
 
-
+;; Loads private configuration files
+(org-babel-load-file
+ (expand-file-name "~/dotfiles/private/emacs/mu4e-contexts.org"))
+(org-babel-load-file
+ (expand-file-name "~/dotfiles/private/emacs/rss-feeds.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
  '(custom-safe-themes
    (quote
-    ("2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(deft-directory "~/org/zettelkasten")
- '(deft-extensions (quote ("org" "md" "txt")))
- '(org-agenda-files
-   (quote
-    ("~/uni/canvas.org" "~/org/wallpen.org" "~/org/birthdays.org" "~/org/gtd.org" "~/uni/index.org")))
- '(package-selected-packages
-   (quote
-    (bbdb gruvbox-theme company lispy company-mode evil-surround evil use-package diminish)))
- '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#282828"))))
+    ("aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" default)))
+ '(org-agenda-files (quote ("~/org/wallpen.org" "~/org/gtd.org")))
+ '(package-selected-packages (quote (evil use-package diminish))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:foreground "#586e75" :weight bold))))
- '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16")))))
+ )
